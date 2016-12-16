0cf98a9b029ec6da08c2834a2bfa5c4fd864b062
Christophe Coenraets <ccoenraets@ccoenraets-ltm1.internal.salesforce.com>
2016-12-16 11:09:06 -0500

    code formatting

--- src/aura/CounterFactoryClient/CounterFactoryClientController.js
+++ src/aura/CounterFactoryClient/CounterFactoryClientController.js
@@ -1,15 +1,15 @@
 ({
 	afterScriptsLoaded : function(component, event, helper) {
-        component.set("v.counter", new Counter());
+		component.set("v.counter", new Counter());
 	},
 
-    getValue : function(component, event, helper) {
-        var counter = component.get("v.counter");
+	getValue : function(component, event, helper) {
+		var counter = component.get("v.counter");
 		component.set("v.value", counter.getValue());
 	},
     
 	increment : function(component, event, helper) {
-        var counter = component.get("v.counter");
+		var counter = component.get("v.counter");
 		component.set("v.value", counter.increment());
 	}
 })
