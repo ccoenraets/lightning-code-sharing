6a91def8f7c597410669506448d25e23dde3e177
Christophe Coenraets <ccoenraets@ccoenraets-ltm1.internal.salesforce.com>
2016-12-16 11:22:51 -0500

    code formatting

--- src/aura/AccountServiceLibClient/AccountServiceLibClientController.js
+++ src/aura/AccountServiceLibClient/AccountServiceLibClientController.js
@@ -1,11 +1,11 @@
 ({
 	afterScriptsLoaded: function(component, event, helper) {
-		
-        var service = new AccountService(component);
-        service.findAll($A.getCallback(function(error, data) {
-            // TODO: Implement proper error handling
-        	component.set("v.accounts", data);                           
+
+		var service = new AccountService(component);
+		service.findAll($A.getCallback(function(error, data) {
+			// TODO: Implement proper error handling
+			component.set("v.accounts", data);                           
 		}));
-        
+
 	}
 })
